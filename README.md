da12f3475bd23c62c013dd2ccd84c877c8581316
Arnab Chattopadhyay <59999832+akbc1221@users.noreply.github.com>
2021-05-25 01:39:47 +0530

    server side commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Memories App Backend #
+
+## Introduction
+
+Created using Node.js, Express & MongoDB (MERN) and is deployed on heroku. The App is called "Memories" beacuse it is a simple social media app that allows users to post interesting events that happened in their lives.
+
+
+## Setup: ##
+- run ```npm i && npm start``` for server side to start the app
+
+## Visit App ##
+<a href="https://memories-post.netlify.app/">Click here</a>
+
---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(express.json({limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/',(req, res)=>{
+    res.send("Welcome to memories app");
+})
+
+//ORM config
+mongoose.connect(process.env.CONNECTION_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server running on port: ${PORT}`)
+    })
+}).catch((err)=>{
+    console.log(err.message)
+});
+
+//to prevent warnings
+mongoose.set('useFindAndModify',false);
+
